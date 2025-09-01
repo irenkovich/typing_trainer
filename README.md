28188fca8febc9462d5f558c9b49ee94235d7f88
irenkovich <irenkovich@gmail.com>
2025-09-01 22:03:29 +0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+### Typing trainer 
+
+This repo contains code for training typing in the terminal.
+Initially, it was made for my girlfriend to learn type in Russian in blind, because her new keyboard doesn't contain Russian at all. :)
+But it can be easily be reworked for other languages/type of texts.
+
+By changing src/data/content.txt you will change values to type.
+By changing src/data/hints.txt you will change hints showed up by backspace pressing.
+
---
+++ src/main.rs
@@ -0,0 +1,215 @@
+use std::{
+    collections::HashMap,
+    fmt::{self, Display},
+    process::Command,
+    time::Instant,
+};
+
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use rand::Rng;
+
+fn main() {
+    clear_terminal();
+    let mut content = load_content();
+
+    let hint_map = load_hints();
+
+    let mut counter = 0;
+    let mut counter_in_row = 0;
+    let mut rng = rand::rng();
+
+    let mut should_print_hint = false;
+
+    let start = Instant::now();
+    'main_loop: while !content.is_empty() {
+        let rand_index = Rng::random_range(&mut rng, 0..content.len());
+        let mut rand_el = content.get(rand_index).unwrap().clone();
+
+        while !rand_el.is_empty() {
+            clear_terminal();
+
+            print_instructions(counter, counter_in_row, &rand_el);
+
+            let target_char = rand_el.chars().next().unwrap();
+
+            if should_print_hint {
+                print_hint(&hint_map, target_char);
+            }
+
+            let continue_to_train = process_user_input(
+                &mut content,
+                &mut counter,
+                &mut counter_in_row,
+                &mut should_print_hint,
+                rand_index,
+                &mut rand_el,
+                target_char,
+            );
+
+            if !continue_to_train {
+                break 'main_loop;
+            }
+        }
+    }
+    let elapsed_secs = start.elapsed().as_secs();
+
+    println!("You were learning for {elapsed_secs} seconds!");
+    println!("Great job!");
+}
+
+//return false on exit
+fn process_user_input(
+    content: &mut Vec<String>,
+    counter: &mut i32,
+    counter_in_row: &mut i32,
+    should_print_hint: &mut bool,
+    rand_index: usize,
+    rand_el: &mut String,
+    target_char: char,
+) -> bool {
+    listen_keys_pressing();
+
+    let continue_to_play = if let Event::Key(key_event) = event::read().unwrap() {
+        match key_event.code {
+            KeyCode::Esc => false,
+            KeyCode::Char(c) => {
+                let guess = c.to_lowercase().next().unwrap();
+                if guess == target_char {
+                    *counter += 1;
+                    *rand_el = rand_el.chars().skip(1).collect();
+                    *counter_in_row += 1;
+                    *should_print_hint = false;
+                    content.remove(rand_index);
+                } else {
+                    *counter_in_row = 0;
+                }
+                true
+            }
+            KeyCode::Backspace => {
+                *should_print_hint = true;
+                true
+            }
+            _ => true,
+        }
+    } else {
+        true
+    };
+
+    stop_listening_keys_pressing();
+
+    continue_to_play
+}
+
+fn stop_listening_keys_pressing() {
+    disable_raw_mode()
+        .unwrap_or_else(|_| panic!("Unexpected error on stop waiting for keys pressing"));
+}
+
+fn listen_keys_pressing() {
+    enable_raw_mode().unwrap_or_else(|_| panic!("unexpected error on waiting for key pressing"));
+}
+
+fn print_hint(hint_map: &HashMap<char, String>, target_char: char) {
+    let hint = hint_map
+        .get(&target_char)
+        .map_or(String::from("No hint for this :("), |x| x.clone());
+    println!("{hint}");
+}
+
+fn print_instructions(counter: i32, counter_in_row: i32, rand_el: &String) {
+    let sweet_words = get_sweet_words_for_typer(counter_in_row);
+    println!("Press esc for exit, backspace for hint.");
+    println!();
+    println!("{sweet_words}");
+    println!("Scores: {counter}.");
+    println!();
+    println!("{rand_el}");
+}
+
+fn get_sweet_words_for_typer(counter_in_row: i32) -> String {
+    match counter_in_row {
+        3..=50 => format!("Wow! {counter_in_row} in a row! Let's do it!"),
+        51..=100 => format!("{counter_in_row} in a row! You know something, don't you?"),
+        101..=200 => format!("Proffesional typer! {counter_in_row} in a row!"),
+        201..=500 => format!("SOMEONE STOP THIS KEYS-REAPER! {counter_in_row} IN A ROW!"),
+        501..30000 => {
+            format!(
+                "Don't you want to relax a bit, perfectionsim-cultist? {counter_in_row} in a row."
+            )
+        }
+        _ => String::new(),
+    }
+}
+
+fn load_content() -> Vec<String> {
+    let content: Vec<String> = include_str!("data/content.txt")
+        .lines()
+        .map(|x| x.to_lowercase())
+        .collect();
+
+    if content.is_empty() {
+        panic!("No content to process, chech 'data/content.txt' file")
+    }
+
+    content
+}
+
+enum HintLoadError {
+    InvalidFormat(usize, String),
+    EmptyKey(usize),
+    EmptyValue(usize),
+}
+
+impl Display for HintLoadError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let msg = match self {
+            Self::InvalidFormat(line, value) => {
+                format!("invalid hint on loading. {line} line: {value}")
+            }
+            Self::EmptyKey(line) => format!("empty hint key on {line} line"),
+            Self::EmptyValue(line) => format!("empty hint value on {line} line"),
+        };
+
+        write!(f, "{msg}")
+    }
+}
+
+fn load_hints() -> HashMap<char, String> {
+    let hints: Result<HashMap<char, String>, HintLoadError> = include_str!("data/hints.txt")
+        .lines()
+        .enumerate()
+        .map(|(line_number, line_value)| {
+            let (key, value) = line_value
+                .split_once(":")
+                .ok_or_else(|| HintLoadError::InvalidFormat(line_number, line_value.to_string()))?;
+
+            let key = key
+                .chars()
+                .next()
+                .ok_or(HintLoadError::EmptyKey(line_number))?;
+
+            if value.is_empty() {
+                Err(HintLoadError::EmptyValue(line_number))?;
+            }
+
+            let value = value.to_string();
+
+            Ok((key, value))
+        })
+        .collect();
+
+    hints.unwrap_or_else(|e| panic!("Error on loading hints: {e}"))
+}
+
+fn clear_terminal() {
+    let status = if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "cls"]).status()
+    } else {
+        Command::new("clear").status()
+    };
+
+    status.unwrap_or_else(|e| panic!("Error on terminal clearing: {e}"));
+}
